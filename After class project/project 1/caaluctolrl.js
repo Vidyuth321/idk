c44bb496d390ca976a3d9fc65b596d5df551674d
Vidyuth321 <divyacb@outlook.com>
2026-05-16 16:40:58 -0700

    Add files via upload

---
+++ After class project/project 1/caaluctolrl.js
@@ -0,0 +1,14 @@
+function getHistory(){
+    return document.getElementById ("history-value").innerText;
+}
+function printHistory(num) {
+     document.getElementById("history-value").innerText=num;
+}
+function getHistory() {
+    return document.getElementById("output-value").innertext;
+}
+function printOutput() {
+    if(num==""){
+        document.getElementById("output-value").innertext;
+    }
+}
